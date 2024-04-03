cef3821c85678d1f50896cfff70a9f796b9f2557
Leejeahwang <89955462+Leejeahwang@users.noreply.github.com>
2024-04-03 12:17:32 +0900

    Update setup.py
    111

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import setuptools
 setuptools.setup(
   name = 'mypackage0403',
   packages = ['mypackage'],
-  url = 'https://github.com/Leejeahwang/mypackage.git'
+  url = "https://github.com/Leejeahwang/mypackage",
+  download_url = "hhttps://github.com/leejeahwang/mypackage/archive/refs/tags/v0.0.1.zip"
   classifiers = [
       "Programming Language :: Python :: 3",
       "Operating System :: OS Indeppendent",
